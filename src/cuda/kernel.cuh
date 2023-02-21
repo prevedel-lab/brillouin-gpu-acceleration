39d7dd6896b20085a1422394af9fd5383e3af5df
Sebastian Hambura <sebastian.hambura@embl.de>
2023-02-21 10:34:56 +0100

    Adding GPL3 licence notice

--- src/cuda/kernel.cuh
+++ src/cuda/kernel.cuh
@@ -10,9 +10,24 @@
  *  - LaunchKernel, a wrapper to take care of the kernel's grid and block size.
  * This allows to launch kernela from regular C++ files.
  *
- * \author Sebastian
+ * \author Sebastian Hambura
  * \date   August 2020
  *********************************************************************/
+ /* Copyright (C) 2020  Sebastian Hambura
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation; either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  */
 
 #pragma once
 
